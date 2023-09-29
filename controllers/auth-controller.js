7c56eabf188a4cfa49faed2e42d28316032ddf15
Andrii Kruk <119102553+chodgi71@users.noreply.github.com>
2023-09-29 15:27:47 +0200

    add users/avatars route

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,23 +1,40 @@
+import fs from "fs/promises";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import gravatar from "gravatar";
+import path from "path";
 
 import { ctrlWrapper } from "../decorators/index.js";
 import { HttpError } from "../helpers/index.js";
 
 import User from "../models/User.js";
 
+const avatarsPath = path.resolve("public", "avatars");
+
 const signup = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
+  const avatarURL = gravatar.url(
+    email,
+    { protocol: "https", s: "250", d: "mm" },
+    true
+  );
+
   if (user) throw HttpError(409, "Email already exist.");
 
   const hashPassword = await bcrypt.hash(password, 10);
 
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
-  res
-    .status(201)
-    .json({ email: newUser.email, subscription: newUser.subscription });
+  res.status(201).json({
+    email: newUser.email,
+    subscription: newUser.subscription,
+    avatar: newUser.avatarURL,
+  });
 };
 
 const signin = async (req, res) => {
@@ -63,10 +80,28 @@ const updateCurrent = async (req, res) => {
   res.json({ message: `user '${email}' is now '${subscription}'` });
 };
 
+const updateAvatar = async (req, res) => {
+  const { email } = req.user;
+  const { path: oldPath, filename } = req.file;
+
+  const newPath = path.join(avatarsPath, filename);
+  await fs.rename(oldPath, newPath);
+
+  const avatarURL = path.join("avatars", filename);
+
+  const result = await User.findOneAndUpdate(
+    { email },
+    { avatarURL },
+    { new: true }
+  );
+  res.json(result);
+};
+
 export default {
   signup: ctrlWrapper(signup),
   signin: ctrlWrapper(signin),
   signout: ctrlWrapper(signout),
   current: ctrlWrapper(current),
   updateCurrent: ctrlWrapper(updateCurrent),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
